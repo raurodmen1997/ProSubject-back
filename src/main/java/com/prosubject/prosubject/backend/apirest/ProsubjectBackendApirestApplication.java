ae2de21b2dd1ff3087147debf8fdc258a10519b8
Raul <raulrodriguezmendez1997@gmail.com>
2020-02-28 11:43:33 +0100

    Commit inicio del proyecto

---
+++ src/main/java/com/prosubject/prosubject/backend/apirest/ProsubjectBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.prosubject.prosubject.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProsubjectBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProsubjectBackendApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prosubject/prosubject/backend/apirest/ProsubjectBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosubject.prosubject.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProsubjectBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
