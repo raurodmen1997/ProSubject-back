e8aced9d2fd246a2257b5db050244f85c2f7ceec
Raul <raulrodriguezmendez1997@gmail.com>
2020-03-09 09:17:01 +0100

    Cross Origin para los distintos end points

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/universidadController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/universidadController.java
@@ -3,6 +3,7 @@ package com.prosubject.prosubject.backend.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.prosubject.prosubject.backend.apirest.service.UniversidadService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class UniversidadController {
 	
 	@Autowired
