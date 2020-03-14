43040ca9ade17916fe57403be37ee4a9eaa30286
Domin-97 <dominmues@gmail.com>
2020-03-14 21:26:20 +0100

    Login v2

--- src/main/java/com/prosubject/prosubject/backend/apirest/controller/LoginController.java
+++ src/main/java/com/prosubject/prosubject/backend/apirest/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.prosubject.prosubject.backend.apirest.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,21 +13,47 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prosubject.prosubject.backend.apirest.model.Administrador;
+import com.prosubject.prosubject.backend.apirest.model.Alumno;
+import com.prosubject.prosubject.backend.apirest.model.Authority;
 import com.prosubject.prosubject.backend.apirest.model.Espacio;
+import com.prosubject.prosubject.backend.apirest.model.Profesor;
+import com.prosubject.prosubject.backend.apirest.model.UserAccount;
 import com.prosubject.prosubject.backend.apirest.repository.EspacioRepository;
-import com.prosubject.prosubject.backend.apirest.service.LoginService;
+import com.prosubject.prosubject.backend.apirest.repository.UserAccountRepository;
+
 
 @RestController
 @RequestMapping("/api/login")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = { "http://localhost:4200" })
 public class LoginController {
-	
+
 	@Autowired
-	private LoginService loginService;
-	
+	private UserAccountRepository userAccountRepository;
+
 	@GetMapping("")
-	public Long logearse(@RequestParam String username,@RequestParam String password){
-		return this.loginService.logearse(username, password);
+	public ResponseEntity<?> logearse(@RequestParam String username, @RequestParam String password) {
+		Administrador administrador = null;
+		Alumno alumno = null;
+		Profesor profesor = null;
+		
+
+		UserAccount a = this.userAccountRepository.cuentaLogueada(username, password);
+//		System.out.println(a.getAutoridad());
+//		System.out.println(Authority.ADMIN);
+		
+		if (a == null) {
+			return null;
+		} else if (a.getAutoridad() == Authority.ADMIN) {
+			administrador = this.userAccountRepository.findAdministradorByUserAccountId(a.getId());
+			return new ResponseEntity<Administrador>(administrador, HttpStatus.OK);
+		} else if (a.getAutoridad() == Authority.PROFESOR) {
+			profesor = this.userAccountRepository.findProfesorByUserAccountId(a.getId());
+			return new ResponseEntity<Profesor>(profesor, HttpStatus.OK);
+		} else {
+			alumno = this.userAccountRepository.findAlumnoByUserAccountId(a.getId());
+			return new ResponseEntity<Alumno>(alumno, HttpStatus.OK);
+		}
+
 	}
 
 }
