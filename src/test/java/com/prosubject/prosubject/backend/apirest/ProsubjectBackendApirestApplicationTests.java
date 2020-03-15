2209aa581e1e0dc5e8354a9031921573357c96dd
alvcascac <acastroc@hotmail.es>
2020-03-15 16:33:50 +0100

    selenium prueba

--- src/test/java/com/prosubject/prosubject/backend/apirest/ProsubjectBackendApirestApplicationTests.java
+++ src/test/java/com/prosubject/prosubject/backend/apirest/ProsubjectBackendApirestApplicationTests.java
@@ -1,13 +1,57 @@
 package com.prosubject.prosubject.backend.apirest;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.net.URL;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
-class ProsubjectBackendApirestApplicationTests {
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
-	@Test
-	void contextLoads() {
-	}
+import java.io.File;
+import java.net.URL;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+class ProsubjectBackendApirestApplicationTests{
+	
+	private SeleniumExample seleniumExample;
+    private String expectedTitle = "Baeldung | Java, Spring and Web Development tutorials";
+
+    @Before
+    public void setUp() {
+        seleniumExample = new SeleniumExample();
+    }
+
+    @After
+    public void tearDown() {
+        seleniumExample.closeWindow();
+    }
+
+    @Test
+    public void whenAboutBaeldungIsLoaded_thenAboutEugenIsMentionedOnPage() {
+        seleniumExample.getAboutBaeldungPage();
+        String actualTitle = seleniumExample.getTitle();
+        assertNotNull(actualTitle);
+        assertEquals(expectedTitle, actualTitle);
+        assertTrue(seleniumExample.isAuthorInformationAvailable());
+    }
+	
 }
